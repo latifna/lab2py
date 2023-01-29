e48418871acc8e53716966218237ccb4f3b1ba05
latif <latifnuranhari@gmail.com>
2023-01-29 15:42:57 +0700

    update

--- file/latihan3.py
+++ file/latihan3.py
@@ -1,25 +1,16 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # input nilai variabel
->>> a=input ("Masukkan nilai a:")
-Masukkan nilai a:5
->>> b=input ("Masukkan nilai b:")
-Masukkan nilai b:6
->>> 
->>> # input nilai variabel
->>> print ("Variabel a=",a)
-Variabel a= 5
->>> print ("Variabel b=",b)
-Variabel b= 6
->>> 
->>> # cetak hasil operasi kedua variabel dengan string format
->>> print ("Hasil penggabungan=", a+b)
-Hasil penggabungan= 56
->>> 
->>> # konversi nilai variabel
->>> a=int (a)
->>> b=int (b)
->>> print ("Hasil penjumlahan=", a+b)
-Hasil penjumlahan= 11
->>> print ("Hasil penjumlahan=", a/b)
-Hasil penjumlahan= 0.8333333333333334
+# input nilai variabel
+a=input ("Masukkan nilai a:")
+b=input ("Masukkan nilai b:")
+
+# input nilai variabel
+print ("Variabel a=",a)
+print ("Variabel b=",b)
+
+# cetak hasil operasi kedua variabel dengan string format
+print ("Hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+
+# konversi nilai variabel
+a= int (a)
+b= int (b)
+print ("Hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print ("Hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))

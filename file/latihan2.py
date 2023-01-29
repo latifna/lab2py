1be01bcdef7eb0c62b75d1659afb03863f55ca08
latif <latifnuranhari@gmail.com>
2023-01-29 12:10:28 +0700

    Menambahkan file

---
+++ file/latihan2.py
@@ -0,0 +1,10 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= 8
+>>> b= 6
+>>> print ("Variable a=" ,a)
+Variable a= 8
+>>> print ("Variable b=" ,b)
+Variable b= 6
+>>> print ("Hasil penjumlahan a+b=" ,a+b)
+Hasil penjumlahan a+b= 14
---
+++ file/latihan3.py
@@ -0,0 +1,25 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # input nilai variabel
+>>> a=input ("Masukkan nilai a:")
+Masukkan nilai a:5
+>>> b=input ("Masukkan nilai b:")
+Masukkan nilai b:6
+>>> 
+>>> # input nilai variabel
+>>> print ("Variabel a=",a)
+Variabel a= 5
+>>> print ("Variabel b=",b)
+Variabel b= 6
+>>> 
+>>> # cetak hasil operasi kedua variabel dengan string format
+>>> print ("Hasil penggabungan=", a+b)
+Hasil penggabungan= 56
+>>> 
+>>> # konversi nilai variabel
+>>> a=int (a)
+>>> b=int (b)
+>>> print ("Hasil penjumlahan=", a+b)
+Hasil penjumlahan= 11
+>>> print ("Hasil penjumlahan=", a/b)
+Hasil penjumlahan= 0.8333333333333334
